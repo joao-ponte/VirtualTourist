2af5ac9f9c2d0a4f055fa7b1e5195f4d6646b620
Joao Ponte <joao_ponte@msn.com>
2023-07-19 15:00:15 +0100

    Implement VirtualTourist data model with entities: Pin, Album, and Image, including necessary relationships. Create TravelLocationViewController and ViewModel.

---
+++ Virtual Tourist/Travel Location/TravelLocationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TravelLocationViewController.swift
+//  Virtual Tourist
+//
+//  Created by João Ponte on 19/07/2023.
+//
+
+import Foundation
+import MapKit
+
+class TravelLocationViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ Virtual Tourist/Travel Location/TravelLocationViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TravelLocationViewModel.swift
+//  Virtual Tourist
+//
+//  Created by João Ponte on 19/07/2023.
+//
+
+import Foundation
+
+class TravelLocationViewModel {
+    
+}
