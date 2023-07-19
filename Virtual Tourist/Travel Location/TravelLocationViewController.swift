880892fcfb2b0ff269dd11c52c0e4805382e817d
Joao Ponte <joao_ponte@msn.com>
2023-07-19 16:13:08 +0100

    Add TravelLocationViewController and handle pin creation on the map Create TravelLocationViewModel for managing pins' coordinates Updated files for pin functionality in Virtual Tourist project

--- Virtual Tourist/Travel Location/TravelLocationViewController.swift
+++ Virtual Tourist/Travel Location/TravelLocationViewController.swift
@@ -5,15 +5,57 @@
 //  Created by João Ponte on 19/07/2023.
 //
 
-import Foundation
+import UIKit
 import MapKit
 
-class TravelLocationViewController: UIViewController {
+class TravelLocationViewController: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
+    var viewModel: TravelLocationViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        viewModel = TravelLocationViewModel()
+        mapView.delegate = self
+        
+        setupMapGesture()
     }
     
+    private func setupMapGesture() {
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(_:)))
+        longPressGesture.minimumPressDuration = 0.5 // Adjust as needed
+        mapView.addGestureRecognizer(longPressGesture)
+    }
+    
+    @objc private func handleLongPress(_ gestureRecognizer: UILongPressGestureRecognizer) {
+        if gestureRecognizer.state == .began {
+            handleLongPressBegan(gestureRecognizer)
+        }
+    }
+    
+    private func handleLongPressBegan(_ gestureRecognizer: UILongPressGestureRecognizer) {
+        let touchPoint = gestureRecognizer.location(in: mapView)
+        let coordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        
+        addPin(at: coordinate)
+    }
+    
+    private func addPin(at coordinate: CLLocationCoordinate2D) {
+        viewModel.addPin(at: coordinate)
+        
+        // Add the new pin to the map
+        let newPin = MKPointAnnotation()
+        newPin.coordinate = coordinate
+        mapView.addAnnotation(newPin)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // Handle custom annotation views if needed
+        return nil
+    }
 }
+
+
+
+
--- Virtual Tourist/Travel Location/TravelLocationViewModel.swift
+++ Virtual Tourist/Travel Location/TravelLocationViewModel.swift
@@ -6,7 +6,18 @@
 //
 
 import Foundation
+import MapKit
 
 class TravelLocationViewModel {
+    
+    var pins: [MKPointAnnotation] = []
+    
+    func addPin(at coordinate: CLLocationCoordinate2D) {
+        let newPin = MKPointAnnotation()
+        newPin.coordinate = coordinate
+        pins.append(newPin)
+        
+        print(pins)
+    }
     
 }
